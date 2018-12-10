d700425a75f0d0d622e70e59a686736b525228aa
nrivkin <noah.rivkin@students.olin.edu>
2018-12-10 07:47:09 -0500

    Wire most of the pulse channel

--- pulseChannel.v
+++ pulseChannel.v
@@ -1,43 +1,86 @@
 `include "rectangle.v"
 `include "frequency.v"
-`include "envelope.v"
+`include "commonComponents.v"
 
 module pulseChannel(
     input clk,
+    input iLength_clk,
+    input iEnvelope_clk,
+    input iSweep_clk,
     input [7:0] reg4000,
     input [7:0] reg4001,
     input [7:0] reg4002,
-    input [0:7] reg4003,
-    output [3:0] reg pulse
+    input [7:0] reg4003,
+    output reg[3:0] pulse
 ); 
 
-reg [10:0] timer;
-reg [1:0] duty_cycle;
-reg counter_halt;
-reg envelope;
-reg [3:0] volume;
-reg sweep_enable;
-reg [2:0] period;
-reg negate;
-reg [2:0] shift;
-reg [4:0] counter_load;
+wire [10:0] timer;
+wire [1:0] duty_cycle;
+wire counter_halt;
+wire constant_volume;
+wire [3:0] volume_period;
+wire sweep_enable;
+wire [2:0] period;
+wire negate;
+wire [2:0] shift;
+wire [4:0] counter_load;
+wire frequency_data;
+wire rectangle_out;
+wire [3:0] envelope_volume;
+wire [6:0] length_out;
 
 assign timer = {reg4003[2:0], reg4002[7:0]};
 assign duty_cycle = reg4000[7:6];
-assign envelope = reg4000[5];
+assign constant_volume = reg4000[5];
 assign counter_halt = reg4000[4];
-assign volume = reg4000[3:0];
+assign volume_period = reg4000[3:0];
 assign sweep_enable = reg4001[7];
 assign period = reg4001[6:4];
 assign negate = reg4001[3];
 assign shift = reg4001[2:0];
-assign counter_load <= reg4003[7:3];
+assign counter_load = reg4003[7:3];
 
-rectangle myRectangle();
-frequency myFrequency();
-envelope myEnvelope();
+reg rectangle_reset;
+reg sweep_reset;
+reg envelope_reset;
+reg length_reset;
+
+rectangle myRectangle(
+    .oData(rectangle_out),
+    .clk(clk),
+    .iReset(rectangle_reset),
+    .iEnable(frequency_data),
+    .iDuty_cycle_type(duty_cycle)
+);
+frequency myFrequency(
+    .oData(frequency_data),
+    .clk(clk),
+    .iSweep_clk(iSweep_clk),
+    .iSweep_reset(sweep_reset),
+    .iSweep_enable(sweep_enable),
+    .iSweep_refresh_rate(),  // TODO
+    .iSweep_mode(negate),
+    .iSweep_shift(shift),
+    .iPeriod_reset(),  // TODO
+    .iPeriod(timer)
+);
+envelope myEnvelope(
+    .clk(clk),
+    .loop(counter_halt),
+    .disableFlag(constant_volume),
+    .resetFlag(envelope_reset),
+    .n(volume_period),
+    .volume(envelope_volume)
+);
+lengthCounter myLengthCounter(
+    .clk(clk),
+    .halt(counter_halt),
+    .counterLoad(counter_load),
+    .out(length_out)
+);
 
 always @(posedge clk) begin
+    // update flags as needed
 end
 
 endmodule
